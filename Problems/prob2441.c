2ff06edfb19858226facdadffba276ce9e440384
NaMooJoon <jjag1015@naver.com>
2021-02-14 17:37:12 +0900

    prob2441.c

---
+++ Problems/prob2441.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int N;
+    int i, j;
+
+    scanf("%d", &N);
+
+    for(i = 0; i < N; i++) {
+        for(j = 0; j < i; j++)
+            putchar(' ');
+        for(j = 0; j < (N - i); j++)
+            putchar('*');
+        putchar('\n');
+    }
+
+
+    return 0;
+}
