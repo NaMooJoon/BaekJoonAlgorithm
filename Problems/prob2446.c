53439f684c5a459c23c283c3516b09b9ed2cb4c1
NaMooJoon <jjag1015@naver.com>
2021-02-14 23:27:40 +0900

    prob2446.c

---
+++ Problems/prob2446.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+    int N;
+    int i, j;
+
+    scanf("%d", &N);
+    for(i = 0; i < N; i++) {
+        for(j = 0; j < i; j++)
+            putchar(' ');
+        for(j = 0; j < 2 * (N - i) - 1; j++)
+            putchar('*');
+        putchar('\n');
+    }
+    for(i = N - 2; i >= 0; i--) {
+        for(j = 0; j < i; j++)
+            putchar(' ');
+        for(j = 0; j < 2 * (N - i) - 1; j++)
+            putchar('*');
+        putchar('\n');
+    }
+
+    return 0;
+}
